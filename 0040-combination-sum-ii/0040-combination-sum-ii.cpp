2d9e93a3e057d9a0f60429934119efb87146985b
manishkrsh <111191351+manishkrsh@users.noreply.github.com>
2025-07-06 12:21:26 +0530

    Time: 0 ms (100%), Space: 14.1 MB (60.81%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
-    void getAllCombinations(vector<int>& arr, int tar, int idx, vector<int> &combin, vector<vector<int>> &ans){
-        if(tar == 0){
-            ans.push_back(combin);
-            return;
-        }
-        for(int i = idx; i < arr.size(); ++i){
-            if(i > idx && arr[i] == arr[i - 1]) continue; // skip duplicates
-            if(arr[i] > tar) break; // no need to proceed further
-            combin.push_back(arr[i]);
-            getAllCombinations(arr, tar - arr[i], i + 1, combin, ans);
-            combin.pop_back();
-        }
+void getAllCombinations(vector<int>& arr, int tar, int idx, vector<int> &combin, vector<vector<int>> &ans){
+    if(tar == 0){
+        ans.push_back(combin);
+        return;
     }
+    for(int i = idx; i < arr.size(); ++i){
+        if(i > idx && arr[i] == arr[i - 1]) continue; // skip duplicates
+        if(arr[i] > tar) break; // pruning
+        combin.push_back(arr[i]);
+        getAllCombinations(arr, tar - arr[i], i + 1, combin, ans);
+        combin.pop_back();
+    }
+}
 
     vector<vector<int>> combinationSum2(vector<int>& arr, int tar) {
-        sort(arr.begin(), arr.end());
+        sort(arr.begin(),arr.end());
         vector<vector<int>> ans;
         vector<int> combin;
+
         getAllCombinations(arr, tar, 0, combin, ans);
         return ans;
     }
