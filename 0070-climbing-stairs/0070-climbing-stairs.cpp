73fb27697edde09f5be5eccdb6da2a23741b81f9
manishkrsh <111191351+manishkrsh@users.noreply.github.com>
2025-07-16 00:16:05 +0530

    Time: 0 ms (100%), Space: 7.9 MB (72.91%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
-   int climbStairsMemo(int n, vector<int> &dp) {
-    if (n == 0 || n == 1) return 1;        // base case
-
-    if (dp[n] != -1) return dp[n];         // already solved? return from dp
-
-    // store the result in dp before returning
-    dp[n] = climbStairsMemo(n - 1, dp) + climbStairsMemo(n - 2, dp);
-    return dp[n];
-}
-
-int climbStairs(int n) {
-    vector<int> dp(n + 1, -1);  // initialize dp with -1 meaning "not solved yet"
-    return climbStairsMemo(n, dp);
-}
+    int climbStairs(int n) {
+        if(n<3) return n;
+        int a=1,b=2;
+        for(int i=3;i<=n;++i){ 
+            int c=a+b; a=b; b=c; 
+        }
+        return b;
+    }
+    
 };
