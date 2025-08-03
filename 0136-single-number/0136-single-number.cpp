e3c54c2cc41cf9e42185cda61d562cdf602f5c02
manishkrsh <111191351+manishkrsh@users.noreply.github.com>
2025-08-03 17:39:17 +0530

    Time: 0 ms (100%), Space: 20.7 MB (58.01%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+#include <numeric> // For std::accumulate
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int result = 0;
+        for (int num : nums) {
+            result ^= num;
+        }
+        return result;
+    }
+};
