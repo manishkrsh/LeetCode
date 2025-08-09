0c1509bc74863dd04aafdbf0f0dadd12551519d2
manishkrsh <111191351+manishkrsh@users.noreply.github.com>
2025-08-09 13:42:01 +0530

    Time: 0 ms (100%), Space: 9.7 MB (73.82%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,11 +1,18 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        if (s==t){
-            return true;
+        if (s.size() != t.size()) return false;
+
+        int count[26] = {0};
+
+        for (int i = 0; i < s.size(); i++) {
+            count[s[i] - 'a']++;
+            count[t[i] - 'a']--;
         }
-        return false;
+
+        for (int i = 0; i < 26; i++) {
+            if (count[i] != 0) return false;
+        }
+        return true;
     }
 };
