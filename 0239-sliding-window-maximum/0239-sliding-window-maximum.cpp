d59ae88ca9659b4c263f27ad505569638c85ca90
manishkrsh <111191351+manishkrsh@users.noreply.github.com>
2025-08-10 16:26:11 +0530

    Time: 393 ms (6.8%), Space: 216.5 MB (5.26%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,23 +1,13 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        priority_queue<pair<int,int>> pq; // max heap: {value, index}
-        vector<int> ans;
-        int n = nums.size();
-        
-        for (int i = 0; i < n; i++) {
-            pq.push({nums[i], i});
-            
-            // Remove elements that are out of the current window
-            while (pq.top().second <= i - k) {
-                pq.pop();
-            }
-            
-            // Start recording results when we have a full window
-            if (i >= k - 1) {
-                ans.push_back(pq.top().first);
-            }
-        }
-        return ans;
+    multiset<int> window;
+    vector<int> ans;
+    for (int i = 0; i < nums.size(); i++) {
+        window.insert(nums[i]);
+        if (i >= k) window.erase(window.find(nums[i-k]));
+        if (i >= k-1) ans.push_back(*window.rbegin());
     }
+    return ans;
+}
 };
