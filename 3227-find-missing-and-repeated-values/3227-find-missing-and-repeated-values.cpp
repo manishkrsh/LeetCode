749a345ef2e16e0dba0ba84d318117e5939ef094
manishkrsh <111191351+manishkrsh@users.noreply.github.com>
2025-08-11 22:30:08 +0530

    Time: 3 ms (66.16%), Space: 25.6 MB (92.75%) - LeetHub

--- 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.cpp
@@ -2,21 +2,29 @@ class Solution {
 public:
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
         int n = grid.size();
-        int N = n * n;
-        vector<int> count(N + 1, 0);
-        int duplicate = -1, missing = -1;
+        long long N = 1LL * n * n;
+
+        long long S = N * (N + 1) / 2; // expected sum
+        long long SQ = N * (N + 1) * (2 * N + 1) / 6; // expected sum of squares
+
+        long long S_actual = 0, SQ_actual = 0;
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                count[grid[i][j]]++;
+                long long val = grid[i][j];
+                S_actual += val;
+                SQ_actual += val * val;
             }
         }
 
-        for (int i = 1; i <= N; i++) {
-            if (count[i] == 2) duplicate = i;
-            if (count[i] == 0) missing = i;
-        }
+        long long diff = S_actual - S; // x - y
+        long long sq_diff = SQ_actual - SQ; // x^2 - y^2
 
-        return {duplicate, missing};
+        long long sum_xy = sq_diff / diff; // x + y
+
+        long long x = (diff + sum_xy) / 2;
+        long long y = x - diff;
+
+        return {(int)x, (int)y};
     }
 };
