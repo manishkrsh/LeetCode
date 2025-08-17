e09f63e52359ad5da9b86cbebd627a5c62f65123
manishkrsh <111191351+manishkrsh@users.noreply.github.com>
2025-08-17 22:39:10 +0530

    Time: 1 ms (48.34%), Space: 10 MB (58.57%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
-    string filterString(string s) {
-        string result;
-        result.reserve(s.size()); // reserve memory to avoid reallocations
-        for (char c : s) {
-            if (isalnum(c)) {  // keep alphabets & numbers
-                result += tolower(c); // normalize case
-            }
-        }
-        return result;
-    }
-
     bool isPalindrome(string s) {
-        s = filterString(s);
-        int i = 0, j = s.size() - 1;
-        while (i < j) {
-            if (s[i] != s[j]) return false;
-            i++;
-            j--;
+        int left = 0, right = s.size() - 1;
+
+        while (left < right) {
+            // skip non-alphanumeric chars
+            while (left < right && !isalnum(s[left])) left++;
+            while (left < right && !isalnum(s[right])) right--;
+
+            // compare in lowercase
+            if (tolower(s[left]) != tolower(s[right])) {
+                return false;
+            }
+
+            left++;
+            right--;
         }
         return true;
     }
