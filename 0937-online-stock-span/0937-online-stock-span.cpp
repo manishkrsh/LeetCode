99e6f1955127215ca790b9bb288a1867b1994719
manishkrsh <111191351+manishkrsh@users.noreply.github.com>
2025-08-20 16:44:25 +0530

    Time: 43 ms (20.81%), Space: 94.1 MB (5.26%) - LeetHub

--- 0937-online-stock-span/0937-online-stock-span.cpp
+++ 0937-online-stock-span/0937-online-stock-span.cpp
@@ -1,19 +1,31 @@
 class StockSpanner {
 public:
-    // stack to store {price, span}
-    stack<pair<int, int>> st;
+    vector<int> prices;   // store all previous prices
+    stack<int> st;        // stack to store indices of "previous greater element"
 
-    StockSpanner() {
-    }
-    
+    StockSpanner() {}
+
     int next(int price) {
-        int span = 1;  // at least today counts
-        // merge spans of all previous prices <= current
-        while (!st.empty() && st.top().first <= price) {
-            span += st.top().second;
+        prices.push_back(price);   // store today's price
+        int i = prices.size() - 1; // today's index
+
+        // pop until we find a price greater than current
+        while (!st.empty() && prices[st.top()] <= price) {
             st.pop();
         }
-        st.push({price, span});
+
+        int span;
+        if (st.empty()) {
+            // no greater element on left -> span = i+1 (all days so far)
+            span = i + 1;
+        } else {
+            // nearest greater on left is at index st.top()
+            span = i - st.top();
+        }
+
+        // push current index for future queries
+        st.push(i);
+
         return span;
     }
 };
