f33200accc68c964c87e9a1ee07df1269f045d60
Manish Kumar <111191351+manishkrsh@users.noreply.github.com>
2025-10-26 17:16:39 +0530

    Time: 0 ms (100%), Space: 7.8 MB (72.97%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -3,19 +3,19 @@ public:
     int getNext(int n) {
         int totalSum = 0;
         while (n > 0) {
-            int digit = n % 10;
-            totalSum += digit * digit;
+            int d = n % 10;
+            totalSum += d * d;
             n /= 10;
         }
         return totalSum;
     }
 
     bool isHappy(int n) {
-        unordered_set<int> seen;
-        while (n != 1 && !seen.count(n)) {
-            seen.insert(n);
-            n = getNext(n);
+        int slow = n, fast = getNext(n);
+        while (fast != 1 && slow != fast) {
+            slow = getNext(slow);
+            fast = getNext(getNext(fast));
         }
-        return n == 1;
+        return fast == 1;
     }
 };
