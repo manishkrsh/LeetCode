2e5da81062cf3ca89b77534cb0e37793e085c128
Manish Kumar <111191351+manishkrsh@users.noreply.github.com>
2025-10-27 18:43:45 +0530

    Time: 0 ms (100%), Space: 23.2 MB (92.58%) - LeetHub

--- 3311-ant-on-the-boundary/3311-ant-on-the-boundary.cpp
+++ 3311-ant-on-the-boundary/3311-ant-on-the-boundary.cpp
@@ -1,12 +1,9 @@
 class Solution {
 public:
     int returnToBoundaryCount(vector<int>& nums) {
-        int ans=0;
-        int bound=0;
-        for(int i=0; i<nums.size(); i++){
-            bound+=nums[i];
-            if(bound==0)ans++;
-        }
+        int ans = 0, pos = 0;
+        for (int x : nums)
+            if ((pos += x) == 0) ans++;
         return ans;
     }
 };
