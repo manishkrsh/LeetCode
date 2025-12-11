542a01de4c35ff988684161595b83d621ec2f704
Manish Kumar <111191351+manishkrsh@users.noreply.github.com>
2025-12-11 22:36:51 +0530

    Time: 11 ms (23.07%), Space: 14.5 MB (35.28%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     set<vector<int>> s;
     void getCombinations(vector<int> &arr, int tar, int idx, vector<int> &combin, vector<vector<int>> &ans){
-
+        if(idx >= arr.size() || tar<0)return;
         if(tar==0){
             if(s.find(combin)==s.end()){
             ans.push_back(combin);
@@ -10,12 +10,9 @@ public:
             }
             return;  
         }
-        if(tar<0 || idx==arr.size()){
-            return;
-        }
         combin.push_back(arr[idx]);
-        getCombinations(arr, tar-arr[idx], idx+1, combin, ans);
         getCombinations(arr, tar-arr[idx], idx, combin, ans);
+        getCombinations(arr, tar-arr[idx], idx+1, combin, ans);
         combin.pop_back();
         getCombinations(arr, tar, idx+1, combin, ans);
 
@@ -23,7 +20,7 @@ public:
     vector<vector<int>> combinationSum(vector<int>& arr, int target) {
         vector<int> combin;
         vector<vector<int>> ans;
-        getCombinations(arr,target,0,combin,ans);
+        getCombinations(arr, target, 0, combin, ans);
         return ans;
     }
 };
