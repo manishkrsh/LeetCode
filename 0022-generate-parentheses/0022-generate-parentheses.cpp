6bd87e65f45f0f8c1afd79bf823a3bcd14b6804e
Manish Kumar <111191351+manishkrsh@users.noreply.github.com>
2025-12-13 12:25:47 +0530

    Time: 0 ms (100%), Space: 14.2 MB (70.56%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    char o = '(', c = ')';
+    set<string> st;
+
+    void backtrack(string &s, int n, int m) {
+        if (n < 0 || m < 0) return;
+
+        if (n == 0 && m == 0) {
+            st.insert(s);   
+            return;
+        }
+
+        if (n > 0) {
+            s.push_back(o);
+            backtrack(s, n - 1, m);
+            s.pop_back();
+        }
+
+        if (m > 0 && m>n) {
+            s.push_back(c);
+            backtrack(s, n, m - 1);
+            s.pop_back();
+        }
+    }
+
+    vector<string> generateParenthesis(int n) {
+        string s = "";
+        backtrack(s, n, n);
+        return vector<string>(st.begin(), st.end()); // return unique results
+    }
+};
