7f87fba5c1a0bcff66161c908de6db7ed57c85fe
Manish Kumar <111191351+manishkrsh@users.noreply.github.com>
2025-12-13 11:16:04 +0530

    Time: 0 ms (100%), Space: 11.6 MB (17.85%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -6,14 +6,13 @@ public:
         vector<int> sub;
         backtrack(nums, 0, sub, st);
 
-        return vector<vector<int>>(st.begin(), st.end()); // Convert set to vector
+        return vector<vector<int>>(st.begin(), st.end()); 
     }
-
-    void backtrack(vector<int>& nums, int idx, vector<int>& sub, set<vector<int>>& st) {
+    void backtrack(vector<int> nums, int idx, vector<int> &sub, set<vector<int>> &st){
         st.insert(sub);
-        for (int i = idx; i < nums.size(); i++) {
+        for(int i=idx; i<nums.size(); i++){
             sub.push_back(nums[i]);
-            backtrack(nums, i + 1, sub, st);
+            backtrack(nums, i+1, sub, st);
             sub.pop_back();
         }
     }
