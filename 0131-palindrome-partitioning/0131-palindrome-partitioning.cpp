f1d884a8ccee6aad267ad4903aaab92a491efe60
Manish Kumar <111191351+manishkrsh@users.noreply.github.com>
2025-12-14 12:50:30 +0530

    Time: 62 ms (27.87%), Space: 80 MB (21.75%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<string>> ans;
+    vector<string> path;
+
+    bool isPalindrome(const string &s, int l, int r) {
+        while (l < r) {
+            if (s[l++] != s[r--]) return false;
+        }
+        return true;
+    }
+
+    void backtrack(int start, string &s) {
+        // If we've used the whole string, store the partition
+        if (start == s.size()) {
+            ans.push_back(path);
+            return;
+        }
+
+        for (int end = start; end < s.size(); end++) {
+            if (isPalindrome(s, start, end)) {
+                path.push_back(s.substr(start, end - start + 1));
+                backtrack(end + 1, s);
+                path.pop_back(); // backtrack
+            }
+        }
+    }
+
+    vector<vector<string>> partition(string s) {
+        backtrack(0, s);
+        return ans;
+    }
+};
